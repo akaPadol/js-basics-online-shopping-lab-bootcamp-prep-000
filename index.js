e8fcc2c86e652c1df280c0b6013db4294098382c
Antonino Badalamenti <bada.nino95@gmail.com>
2017-09-12 00:14:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,14 +17,55 @@ function setCart(c) {
     + Upon the successful addition of a new item to the cart, the function should print `<itemName> has been added to your cart.` to the console and then `return` the updated `cart`.*/
 
 function addToCart(item) {
- cart[item]=Math.floor((Math.random() * 100) + 1);
+ cart[item]="$"+Math.floor((Math.random() * 100) + 1);
  console.log(item+" has been added to your cart.");
  return cart;
 }
 
+/*2. The `viewCart()` function does not accept any arguments. It should loop over every item in your cart, printing out the contents as one long, coherent statement in this format: `In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.`
+    + If the cart is empty, the function should instead print out `Your shopping cart is empty.`
+    + ***Note***: Pay close attention to the syntax above. The printed statement should be a single sentence that begins with `In your cart, you have `, terminates in a period, and can assume the following shapes according to how many items the cart contains:
+      * 1 item — `In your cart, you have bananas at $17.`
+      * 2 items — `In your cart, you have bananas at $17 and pancake batter at $5.`
+      * 3+ items — `In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.`*/
 function viewCart() {
-  // write your code here
+  var i = countProperties(cart);
+  var res='In your cart, you have '
+  if ( i === 0){return "Your shopping cart is empty."}
+  switch(i) {
+    case 1:
+          for (var key in cart){
+            
+            res+= key+" at "+cart[key]
+          }
+         
+        break;
+    case 2:
+      let count = 0
+    for (var key in cart){
+      
+      res+= key+" at "+cart[key]+(count ===1 ? " and " : "")
+      ++count
+    }
+    
+        break;
+    default:
+      let count = 0
+      for (var key in cart){
+      
+        res+= key+" at "+cart[key]+", "+(count === (i-2) ? " and " : "")
+        ++count
+      } 
+        break;
 }
+res+="."
+}
+
+function countProperties(obj) {
+    return Object.keys(obj).length;
+}
+
+
 
 function total() {
   // write your code here
